56aca3cf07554388fdc4069ea6cd74a866633943
andrea <andreeearg99@gmail.com>
2025-02-11 20:48:35 +0100

    Interfaz inicio sin acabar

--- BibliotecaFX/src/main/java/org/example/bibliotecafx/Gestion/Gestion.java
+++ BibliotecaFX/src/main/java/org/example/bibliotecafx/Gestion/Gestion.java
@@ -2,7 +2,14 @@ package org.example.bibliotecafx.Gestion;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.example.bibliotecafx.DAO.AutorDAOImpl;
 
 import java.io.IOException;
 
@@ -20,31 +27,40 @@ public class Gestion {
     @FXML
     private VBox PrestamoInterfaz;
 
+    @FXML
+    private TextField nombreAutorField;
+    @FXML
+    private TextField nacionalidadAutorField;
+    @FXML
+    private Label resultadoBusquedaLabel;
+
+    private final AutorDAOImpl autorDAO = new AutorDAOImpl();
+
     // Botones
     @FXML
     private void BotonAutor() throws IOException {
         new CargarInterfaz(InicioInterfaz, "/org/example/bibliotecafx/AutorInterfaz.fxml");
     }
 
-   @FXML
+    @FXML
     private void BotonLibro() throws IOException {
         new CargarInterfaz(InicioInterfaz, "/org/example/bibliotecafx/LibroInterfaz.fxml");
-   }
+    }
 
-   @FXML
-   private void BotonPrestamo() throws IOException {
-       new CargarInterfaz(InicioInterfaz, "/org/example/bibliotecafx/PrestamoInterfaz.fxml");
-   }
+    @FXML
+    private void BotonPrestamo() throws IOException {
+        new CargarInterfaz(InicioInterfaz, "/org/example/bibliotecafx/PrestamoInterfaz.fxml");
+    }
 
-   @FXML
-   private void BotonSocio() throws IOException {
-       new CargarInterfaz(InicioInterfaz, "/org/example/bibliotecafx/SocioInterfaz.fxml");
-   }
+    @FXML
+    private void BotonSocio() throws IOException {
+        new CargarInterfaz(InicioInterfaz, "/org/example/bibliotecafx/SocioInterfaz.fxml");
+    }
 
-   @FXML
+    @FXML
     private void BotonSalir() {
         System.exit(0);
-   }
+    }
 
 
     public void listarAutores(ActionEvent actionEvent) {
@@ -60,5 +76,12 @@ public class Gestion {
     }
 
     public void anadirAutor(ActionEvent actionEvent) {
+
+
+    }
+
+    @FXML
+    private void VolverInicioAutor() throws IOException {
+        new CargarInterfaz(AutorInterfaz, "/org/example/bibliotecafx/InicioInterfaz.fxml");
     }
 }
